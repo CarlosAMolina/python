a7a412c7c771ca0c6a6028904dbe50a14229d393
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-01-06 19:38:18 +0100

    refactor(mock exceptions): extract class to file

---
+++ mock/exceptions/src/exceptions.py
@@ -0,0 +1,2 @@
+class CustomError(ZeroDivisionError):
+    pass
--- mock/exceptions/src/main.py
+++ mock/exceptions/src/main.py
@@ -1,10 +1,7 @@
+from src.exceptions import CustomError
 class RunRaiseException:
     def raise_file_exists_error(self):
         raise FileExistsError
 
     def raise_custom_error(self):
-        raise _CustomError
-
-
-class _CustomError(ZeroDivisionError):
-    pass
+        raise CustomError
