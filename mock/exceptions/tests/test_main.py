83b3b9b7d9ff0ac566c9bf88ec2b00978816b87a
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-01-07 20:50:54 +0100

    test(exceptions): refactor rename import

--- mock/exceptions/tests/test_main.py
+++ mock/exceptions/tests/test_main.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from unittest.mock import patch
 
-from src.exceptions import CustomError
+from src.exceptions import CustomError as CustomError_exceptions
 from src.main import _RunRaiseException
 from src.main import CustomError as CustomError_main
 from src.main import run_catch_exception
@@ -13,9 +13,9 @@ class CustomError_test(ZeroDivisionError):
 
 class TestIsinstance(TestCase):
     def test_isinstance_for_all_classes(self):
-        assert isinstance(CustomError(), CustomError)
-        assert isinstance(CustomError_main(), CustomError)
-        assert not isinstance(CustomError_test(), CustomError)
+        assert isinstance(CustomError_exceptions(), CustomError_exceptions)
+        assert isinstance(CustomError_main(), CustomError_exceptions)
+        assert not isinstance(CustomError_test(), CustomError_exceptions)
 
 
 class Test_run_catch_exceptions(TestCase):
@@ -29,7 +29,7 @@ class Test_run_catch_exceptions(TestCase):
 
     @patch("src.main._RunRaiseException.raise_file_exists_error")
     def test_exception_is_catched_if_mocked_with_custom_error(self, mock_raise_exception):
-        mock_raise_exception.side_effect = CustomError
+        mock_raise_exception.side_effect = CustomError_exceptions
         self.assertEqual("Catched CustomError!", run_catch_exception())
 
 
@@ -56,16 +56,16 @@ class Test_RunRaiseException(TestCase):
         is_catched = False
         try:
             _RunRaiseException().raise_custom_error()
-        except CustomError:
+        except CustomError_exceptions:
             is_catched = True
         self.assertTrue(is_catched)
 
     @patch("src.main._RunRaiseException.raise_file_exists_error")
     def test_catch_mocked_custom_exception(self, mock_raise_exception):
         is_catched = False
-        mock_raise_exception.side_effect = CustomError
+        mock_raise_exception.side_effect = CustomError_exceptions
         try:
             _RunRaiseException().raise_file_exists_error()
-        except CustomError:
+        except CustomError_exceptions:
             is_catched = True
         self.assertTrue(is_catched)
