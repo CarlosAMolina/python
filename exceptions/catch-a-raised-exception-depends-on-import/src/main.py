10d05123f488d8638e888fd9b14227fe3132f602
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-01-07 17:45:40 +0100

    refactor(exception): move code to other file

--- exceptions/catch-a-raised-exception-depends-on-import/src/main.py
+++ exceptions/catch-a-raised-exception-depends-on-import/src/main.py
@@ -4,5 +4,33 @@ def run():
     print("Do some stuff with the exception: ", FolderInS3UriError)
 
 
+class CatchResults:
+    def __init__(self):
+        self._catched = set()
+        self._not_catched = set()
+
+    def __repr__(self) -> str:
+        catched_summary = self._get_summary('catched', self._catched)
+        not_catched_summary = self._get_summary('not catched', self._not_catched)
+        return f"{catched_summary}\n{not_catched_summary}"
+
+    def _get_summary(self, type_str: str, values: set) -> str:
+        return f"{type_str} ({len(self._catched)}): {','.join(values)}"
+
+    @property
+    def catched(self) -> set:
+        return self._catched
+
+    @property
+    def not_catched(self) -> set:
+        return self._not_catched
+
+    def add_to_catched(self, name: str):
+        self._catched.add(name)
+
+    def add_to_not_catched(self, name: str):
+        self._not_catched.add(name)
+
+
 if __name__ == "__main__":
     run()
--- exceptions/catch-a-raised-exception-depends-on-import/tests/test_main.py
+++ exceptions/catch-a-raised-exception-depends-on-import/tests/test_main.py
@@ -3,38 +3,11 @@ import unittest
 from src.exceptions import FolderInS3UriError as FolderInS3UriError_exceptions
 
 from src.main import FolderInS3UriError as FolderInS3UriError_main
+from src.main import CatchResults
 
 
 class FolderInS3UriError_test(FileExistsError):
     pass
-
-
-class CatchResults:
-    def __init__(self):
-        self._catched = set()
-        self._not_catched = set()
-
-    def __repr__(self) -> str:
-        catched_summary = self._get_summary('catched', self._catched)
-        not_catched_summary = self._get_summary('not catched', self._not_catched)
-        return f"{catched_summary}\n{not_catched_summary}"
-
-    def _get_summary(self, type_str: str, values: set) -> str:
-        return f"{type_str} ({len(self._catched)}): {','.join(values)}"
-
-    @property
-    def catched(self) -> set:
-        return self._catched
-
-    @property
-    def not_catched(self) -> set:
-        return self._not_catched
-
-    def add_to_catched(self, name: str):
-        self._catched.add(name)
-
-    def add_to_not_catched(self, name: str):
-        self._not_catched.add(name)
 
 
 class TestCatchExceptions(unittest.TestCase):
