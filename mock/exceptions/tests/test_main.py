d91c88225072f1fe20738002b848dfac8202a58b
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-01-07 23:09:20 +0100

    test(mock): add ways to mock absolute import. refactor rename

--- mock/exceptions/tests/test_main.py
+++ mock/exceptions/tests/test_main.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 
 from src.exceptions import CustomError as ExceptionsCustomError
 from src.main import _RunRaiseException
+from src.main import AbsoluteImportCustomError as MainAbsoluteImportCustomError 
 from src.main import RelativeImportCustomError as MainRelativeImportCustomError
 from src.main import run_catch_exception
 
@@ -12,17 +13,19 @@ class Test_run_catch_exceptions(TestCase):
         self.assertEqual("Catched FileExistsError!", run_catch_exception())
 
     @patch("src.main._RunRaiseException.run")
-    def test_exception_is_catched_if_mocked(self, mock_raise_exception):
+    def test_how_catch_built_in_exception_can_be_mocked(self, mock_raise_exception):
         mock_raise_exception.side_effect = ValueError
         self.assertEqual("Catched ValueError!", run_catch_exception())
 
     @patch("src.main._RunRaiseException.run")
-    def test_exception_is_catched_if_mocked_with_custom_error_absolute_import(self, mock_raise_exception):
+    def test_how_catch_custom_exception_absolute_import_can_be_mocked(self, mock_raise_exception):
         mock_raise_exception.side_effect = ExceptionsCustomError
+        self.assertEqual("Catched AbsoluteImportCustomError!", run_catch_exception())
+        mock_raise_exception.side_effect = MainAbsoluteImportCustomError
         self.assertEqual("Catched AbsoluteImportCustomError!", run_catch_exception())
 
     @patch("src.main._RunRaiseException.run")
-    def test_exception_is_catched_if_mocked_with_custom_error_relative_import(self, mock_raise_exception):
+    def test_how_catch_custom_exception_relative_import_can_be_mocked(self, mock_raise_exception):
         mock_raise_exception.side_effect = MainRelativeImportCustomError
         self.assertEqual("Catched RelativeImportCustomError!", run_catch_exception())
 
