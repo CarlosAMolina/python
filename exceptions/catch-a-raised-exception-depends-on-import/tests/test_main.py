cfde1a61e715c67d497c1dfc6ee75fe021de3c12
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-01-07 18:14:37 +0100

    test(exception): refactor extract variable

--- exceptions/catch-a-raised-exception-depends-on-import/tests/test_main.py
+++ exceptions/catch-a-raised-exception-depends-on-import/tests/test_main.py
@@ -23,15 +23,18 @@ class TestCatchExceptions(unittest.TestCase):
 
     def _get_what_imports_can_catch_the_exception(self, exception_to_check) -> CatchResults:
         catch_results = CatchResults()
-        if self._is_exception_catched_by_exception(exception_to_check, FolderInS3UriError_main):
+        exception_catcher = FolderInS3UriError_main
+        if self._is_exception_catched_by_exception(exception_to_check, exception_catcher):
             catch_results.add_to_catched("FolderInS3UriError_main")
         else:
             catch_results.add_to_not_catched("FolderInS3UriError_main")
-        if self._is_exception_catched_by_exception(exception_to_check, FolderInS3UriError_exceptions):
+        exception_catcher = FolderInS3UriError_exceptions
+        if self._is_exception_catched_by_exception(exception_to_check, exception_catcher):
             catch_results.add_to_catched("FolderInS3UriError_exceptions")
         else:
             catch_results.add_to_not_catched("FolderInS3UriError_exceptions")
-        if self._is_exception_catched_by_exception(exception_to_check, FolderInS3UriError_test):
+        exception_catcher = FolderInS3UriError_test
+        if self._is_exception_catched_by_exception(exception_to_check, exception_catcher):
             catch_results.add_to_catched("FolderInS3UriError_test")
         else:
             catch_results.add_to_not_catched("FolderInS3UriError_test")
