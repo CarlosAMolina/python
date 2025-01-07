89216da9435f2afdf8f864a5f10c6eb64ae55958
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-01-07 21:38:49 +0100

    feat(insinsance): add assertions

--- classes/isinstance/how-imports-impact/tests/test_main.py
+++ classes/isinstance/how-imports-impact/tests/test_main.py
@@ -11,5 +11,8 @@ class CustomError_test(FileExistsError):
 
 class TestIsinstance(unittest.TestCase):
     def test_isinstance_has_excpected_behaviour(self):
+        assert id(CustomError_exceptions) == id(CustomError_absolute_main)
+        assert id(CustomError_exceptions) != id(CustomError_relative_main)
+        assert id(CustomError_exceptions()) == id(CustomError_absolute_main()) == id(CustomError_relative_main())
         assert isinstance(CustomError_exceptions(), CustomError_absolute_main)
         assert not isinstance(CustomError_exceptions(), CustomError_relative_main)
