08cb29a5fb5f8bb9ce25b5eb49134134466bd396
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-01-08 12:05:24 +0100

    feat(isinstance): add check alias

--- classes/isinstance/how-imports-impact/no-using-tests/src/main.py
+++ classes/isinstance/how-imports-impact/no-using-tests/src/main.py
@@ -2,6 +2,7 @@ import pathlib
 import sys
 
 from subfolder.exceptions import CustomError as FromSubfolderCustomError
+from subfolder.exceptions import CustomError as BFromSubfolderCustomError
 
 sys.path.append(str(pathlib.Path(__file__).parent.absolute().joinpath("subfolder")))
 
@@ -12,6 +13,8 @@ from exceptions import CustomError as FromFileCustomError
 # https://docs.python.org/3/reference/expressions.html#is-not
 # https://docs.python.org/3/library/functions.html#id
 # https://docs.python.org/3/library/functions.html#isinstance
-assert FromFileCustomError is not FromSubfolderCustomError
+assert FromSubfolderCustomError is BFromSubfolderCustomError  # The alias does not change the object (same id()).
+assert FromFileCustomError is not FromSubfolderCustomError  # The different import changes the object.
+assert isinstance(FromSubfolderCustomError(), BFromSubfolderCustomError)
 assert not isinstance(FromFileCustomError(), FromSubfolderCustomError)
 assert not isinstance(FromSubfolderCustomError(), FromFileCustomError)
