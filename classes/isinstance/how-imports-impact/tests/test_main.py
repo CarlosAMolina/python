80bf34281d7e4369c576517783f8bc50c569586f
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-01-08 10:09:44 +0100

    refactor(isinstance): rename

--- classes/isinstance/how-imports-impact/tests/test_main.py
+++ classes/isinstance/how-imports-impact/tests/test_main.py
@@ -1,8 +1,8 @@
 import unittest
 
 from src.exceptions import CustomError as ExceptionsCustomError
-from src.main import AbsoluteImportCustomError as MainAbsoluteImportCustomError
-from src.main import RelativeImportCustomError as MainRelativeImportCustomError
+from src.main import FromSrcImportCustomError as MainFromSrcImportCustomError
+from src.main import FromExceptionsImportCustomError as MainFromExceptionsImportCustomError
 
 
 class _TestCustomError(FileExistsError):
@@ -12,19 +12,19 @@ class _TestCustomError(FileExistsError):
 class TestCustomError(unittest.TestCase):
     def test_isinstance_has_expected_behaviour(self):
         # When the import is done in the same way in both files, isinstance is True.
-        self.assertTrue(isinstance(ExceptionsCustomError(), MainAbsoluteImportCustomError))
-        self.assertFalse(isinstance(ExceptionsCustomError(), MainRelativeImportCustomError))
+        self.assertTrue(isinstance(ExceptionsCustomError(), MainFromSrcImportCustomError))
+        self.assertFalse(isinstance(ExceptionsCustomError(), MainFromExceptionsImportCustomError))
         self.assertFalse(isinstance(_TestCustomError(), ExceptionsCustomError))
-        self.assertFalse(isinstance(_TestCustomError(), MainAbsoluteImportCustomError))
-        self.assertFalse(isinstance(_TestCustomError(), MainRelativeImportCustomError))
+        self.assertFalse(isinstance(_TestCustomError(), MainFromSrcImportCustomError))
+        self.assertFalse(isinstance(_TestCustomError(), MainFromExceptionsImportCustomError))
 
     def test_id_has_expected_behaviour(self):
         # Not instance.
-        self.assertEqual(id(ExceptionsCustomError), id(MainAbsoluteImportCustomError))
-        self.assertNotEqual(id(ExceptionsCustomError), id(MainRelativeImportCustomError))
+        self.assertEqual(id(ExceptionsCustomError), id(MainFromSrcImportCustomError))
+        self.assertNotEqual(id(ExceptionsCustomError), id(MainFromExceptionsImportCustomError))
         self.assertNotEqual(id(_TestCustomError), id(ExceptionsCustomError))
-        self.assertNotEqual(id(_TestCustomError), id(MainAbsoluteImportCustomError))
-        self.assertNotEqual(id(_TestCustomError), id(MainRelativeImportCustomError))
+        self.assertNotEqual(id(_TestCustomError), id(MainFromSrcImportCustomError))
+        self.assertNotEqual(id(_TestCustomError), id(MainFromExceptionsImportCustomError))
         # Instances.
-        self.assertEqual(id(ExceptionsCustomError()), id(MainAbsoluteImportCustomError()))
-        self.assertEqual(id(ExceptionsCustomError()), id(MainRelativeImportCustomError()))
+        self.assertEqual(id(ExceptionsCustomError()), id(MainFromSrcImportCustomError()))
+        self.assertEqual(id(ExceptionsCustomError()), id(MainFromExceptionsImportCustomError()))
