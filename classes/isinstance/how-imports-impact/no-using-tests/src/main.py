092cce7f2fd49d9d6f4453f38bac13d68c03dbd0
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-01-08 13:05:25 +0100

    feat(isinstance): add analysis why classes are different

--- classes/isinstance/how-imports-impact/no-using-tests/src/main.py
+++ classes/isinstance/how-imports-impact/no-using-tests/src/main.py
@@ -8,7 +8,7 @@ sys.path.append(str(pathlib.Path(__file__).parent.absolute().joinpath("subfolder
 
 from exceptions import CustomError as FromFileCustomError
 
-
+# Show classes are different.
 # Despite the class is imported from the same file, it has a different ID in each different import.
 # https://docs.python.org/3/reference/expressions.html#is-not
 # https://docs.python.org/3/library/functions.html#id
@@ -18,3 +18,13 @@ assert FromFileCustomError is not FromSubfolderCustomError  # The different impo
 assert isinstance(FromSubfolderCustomError(), BFromSubfolderCustomError)
 assert not isinstance(FromFileCustomError(), FromSubfolderCustomError)
 assert not isinstance(FromSubfolderCustomError(), FromFileCustomError)
+
+# Why are different?
+print(FromSubfolderCustomError)  # <class 'subfolder.exceptions.CustomError'>
+print(BFromSubfolderCustomError)  # <class 'subfolder.exceptions.CustomError'>
+print(FromFileCustomError)  # <class 'exceptions.CustomError'>
+# What is the value after `class`?
+# I think that is some value of the module.
+for module_name, module in sys.modules.copy().items():
+    if 'exceptions' in module_name:
+        print(module_name)
