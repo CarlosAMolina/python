b75578ac30758065080ea40b23f7b53fe4e6e8ff
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-03-22 04:51:48 +0100

    feat(array): update comments

--- array/inner_array.py
+++ array/inner_array.py
@@ -1,10 +1,12 @@
-# Access to inner array elements using multiple variables in for loop
+# Accessing inner array elements using multiple variables in the for loop
 
 info_array = [("freeBSD", "demon"), ("linux", "penguin"), ("mac", "apple")]
 
+# x Wrong
 for info in info_array:
     name, logo = info
     print(f"{name} logo: {logo}")
 
+# ✓ Better
 for name, logo in info_array:
     print(f"{name} logo: {logo}")
